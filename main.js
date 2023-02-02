e7f6395a1ec885a49eafceda9e98d4ca592ae4fb
Dāvis Fjodorovs <daavisff@gmail.com>
2023-02-02 12:08:08 +0200

    v0.2 makeImageDirectory function with user input

--- main.js
+++ main.js
@@ -2,18 +2,41 @@ const puppeteer = require('puppeteer');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const { mkdir } = require('fs/promises');
+const { resolve, join } = require('path');
 
-let url = '';
+async function makeImageDirectory(path) {
+    const createImageDirectory = await mkdir(path);
+}
 
-(async () => {
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
 
-    await page.goto(url);
+readline.question('Links: ', link => {
+    console.log(link);
+    readline.close();
+});
 
-    const imgSrc = await page.$eval('.thumbnail img', img => img.src);
 
-    const imgName = await page.$eval('.col-sm-4 h1', h1 => h1.textContent);
+// readline.question('Path: ', answerPath => {
+//     const imageDirectory = join(answerPath, 'images');
+//     makeImageDirectory(imageDirectory).catch(console.error);  
+//     readline.close();
+// });
 
-    await browser.close();
-})();
+// let url = '';
+
+// (async () => {
+//     const browser = await puppeteer.launch({headless:false});
+//     const page = await browser.newPage();
+
+//     await page.goto(url);
+
+//     const imgSrc = await page.$eval('.thumbnail img', img => img.src);
+
+//     const imgName = await page.$eval('.col-sm-4 h1', h1 => h1.textContent);
+
+//     await browser.close();
+// })();

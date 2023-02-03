0edf2f372eb47b90d9ab3a7d67a73b84239c96a1
Dāvis Fjodorovs <daavisff@gmail.com>
2023-02-03 09:07:22 +0200

    Added XLSX, download image, read links from xlsx and other functions

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 const puppeteer = require('puppeteer');
 const http = require('http');
 const https = require('https');
-const fs = require('fs');
-const readline = require('readline').createInterface({
+const fs = require('fs/promises');
+const readline = require('readline/promises').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 const { mkdir } = require('fs/promises');
-const { resolve, join } = require('path');
+const { /*resolve,*/ join } = require('path');
+const xlsx = require('xlsx');
+
 
 async function makeImageDirectory(path) {
     const createImageDirectory = await mkdir(path);
 }
 
+function downloadImage(url, imageName, sequenceNumber, folderPath) {
+    const imagePath = join(folderPath, `${sequenceNumber}-${imageName}.jpg`)
+    const request = https.get(url, async (result) => {
+        try {
+            await fs.writeFile(imagePath, result);
+            fs.
+        } catch (error) {
+            console.log('ERROR: ', error);   
+        }
+    });
+    request.on('error', error => console.log('ERROR: ',error));
+}
 
-readline.question('Links: ', link => {
-    console.log(link);
-    readline.close();
-});
+// (async () => {
+//     const pathXlsx = await readline.question('Path to xlsx file: ');
+//     readline.close();
+//     const x = xlsx.readFile(pathXlsx);
+//     const sheet = x.Sheets.Sheet1;
+//     const links = xlsx.utils.sheet_to_json(sheet, { header: 1 }).map(element => element[0]);
+//     console.log('Links: ', links);
+// })();
 
+// (async () => {
+//     const path = await readline.question('Path: ');
+//     console.log(link);
+//     readline.close();
+// })();
 
 // readline.question('Path: ', answerPath => {
 //     const imageDirectory = join(answerPath, 'images');

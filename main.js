5186743388356ec45ef46d43913577df5dfa4756
Dāvis Fjodorovs <daavisff@gmail.com>
2023-02-03 11:49:45 +0200

    Working prototype

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-const http = require('http');
 const https = require('https');
 const fs = require('fs/promises');
 const readline = require('readline/promises').createInterface({
@@ -7,12 +6,14 @@ const readline = require('readline/promises').createInterface({
     output: process.stdout
 });
 const { mkdir } = require('fs/promises');
-const { /*resolve,*/ join } = require('path');
+const { join } = require('path');
 const xlsx = require('xlsx');
 
-
-async function makeImageDirectory(path) {
-    const createImageDirectory = await mkdir(path);
+function getLinks(path) {
+    const workBook = xlsx.readFile(path);
+    const workSheet = workBook.Sheets.Sheet1;
+    const links = xlsx.utils.sheet_to_json(workSheet, { header: 1 }).map(element => element[0]);
+    return links;
 }
 
 function downloadImage(url, imageName, sequenceNumber, folderPath) {
@@ -20,7 +21,6 @@ function downloadImage(url, imageName, sequenceNumber, folderPath) {
     const request = https.get(url, async (result) => {
         try {
             await fs.writeFile(imagePath, result);
-            fs.
         } catch (error) {
             console.log('ERROR: ', error);   
         }
@@ -28,38 +28,28 @@ function downloadImage(url, imageName, sequenceNumber, folderPath) {
     request.on('error', error => console.log('ERROR: ',error));
 }
 
-// (async () => {
-//     const pathXlsx = await readline.question('Path to xlsx file: ');
-//     readline.close();
-//     const x = xlsx.readFile(pathXlsx);
-//     const sheet = x.Sheets.Sheet1;
-//     const links = xlsx.utils.sheet_to_json(sheet, { header: 1 }).map(element => element[0]);
-//     console.log('Links: ', links);
-// })();
+(async () => {
+    const pathToUrls = await readline.question('Path to XLSX document containing URLs: ');
+    const urls = getLinks(pathToUrls)
 
-// (async () => {
-//     const path = await readline.question('Path: ');
-//     console.log(link);
-//     readline.close();
-// })();
+    const folderPath = await readline.question('Path for image folder: ');
+    readline.close()
+    const imageFolder = join(folderPath, 'images');
+    await mkdir(imageFolder);
 
-// readline.question('Path: ', answerPath => {
-//     const imageDirectory = join(answerPath, 'images');
-//     makeImageDirectory(imageDirectory).catch(console.error);  
-//     readline.close();
-// });
 
-// let url = '';
+    let sequenceNumber = 1;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    for(let url of urls) {
+        await page.goto(url);
+        const imgSrc = await page.$eval('.thumbnail img', img => img.src);
+        const bookName = await page.$eval('.col-sm-4 h1', h1 => h1.textContent);
+        const imgName = bookName.split('.')[0].replaceAll('"', '');
+        downloadImage(imgSrc, imgName, sequenceNumber, imageFolder);
+        sequenceNumber += 1;
+    }
+    await browser.close();
 
-// (async () => {
-//     const browser = await puppeteer.launch({headless:false});
-//     const page = await browser.newPage();
-
-//     await page.goto(url);
-
-//     const imgSrc = await page.$eval('.thumbnail img', img => img.src);
-
-//     const imgName = await page.$eval('.col-sm-4 h1', h1 => h1.textContent);
-
-//     await browser.close();
-// })();
+    console.log('Done!');
+})();
